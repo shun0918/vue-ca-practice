d8cd920e0d9de81e9c5853cde727426ee5a826f8
shun0918 <kkabosu386@gmail.com>
2021-05-26 21:31:41 +0900

    first commit

---
+++ app/vue-ca-practice/src/views/Hoge.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h1>hogehoge</h1>
+    <button @click="increment()">+1</button>
+    <p>{{state.count}}</p>
+  </div>
+</template>
+import { defineComponent } from 'vue'
+import HelloWorld from '@/components/HelloWorld.vue'-
+
+<script lang="ts">
+import { defineComponent, reactive } from 'vue'
+
+interface State {
+  count: number;
+}
+
+export default defineComponent({
+  name: 'Hoge',
+  setup () {
+    const state = reactive<State>({
+      count: 0
+    })
+    const increment = () => {
+      state.count++
+      console.log(state)
+    }
+
+    return {
+      state,
+      increment
+    }
+  }
+})
+</script>
